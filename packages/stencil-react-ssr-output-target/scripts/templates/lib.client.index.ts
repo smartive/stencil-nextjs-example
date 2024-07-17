7d2aea8e498475467d3ef9acbf3345623451b77d
Thilo Haas <thilo@smartive.ch>
2024-07-17 09:37:04 +0200

    fix: Allow AbcWrapper to be renamed to actual component prefix

--- packages/stencil-react-ssr-output-target/scripts/templates/lib.client.index.ts
+++ packages/stencil-react-ssr-output-target/scripts/templates/lib.client.index.ts
@@ -1,6 +1,8 @@
+/* eslint-disable */
 // Original code from https://github.com/luwes/wesc
 
 import React, { FC, PropsWithChildren, ReactNode } from 'react';
+// @ts-expect-error - leads only to error in template file
 import { WithSSR } from './with-ssr';
 
 export const WithRSCFallback: FC<PropsWithChildren<{ rsc: ReactNode }>> = ({ rsc, children }) =>
@@ -11,16 +13,15 @@ export const WithRSCFallback: FC<PropsWithChildren<{ rsc: ReactNode }>> = ({ rsc
 // > Attempted to call the default export of ... from the server but it's on the client.
 // > It's not possible to invoke a client function from the server, it can only be rendered
 // > as a Component or passed to props of a Client Component.
-export function AbcWrapper({ children }: PropsWithChildren) {
+export function __PASCAL_CASE_COMPONENTS_PREFIX__Wrapper({ children }: PropsWithChildren) {
   if (typeof window === 'undefined') {
-    // eslint-disable-next-line import/no-unresolved
+    // @ts-expect-error - leads only to error in template file
     return import('./render.js').then(({ render }) => render(resolve(children)));
   }
 
   return children;
 }
 
-/* eslint-disable */
 function resolve(children: any, result: any[] = []) {
   const nodes: any[] = [].concat(children ?? []);
 
